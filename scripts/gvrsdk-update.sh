70b807ceb293d17765e70243d81fd3b145072285
codeanticode <andres.colubri@gmail.com>
2017-01-03 22:41:33 -0500

    rename gvr sdk files

--- scripts/gvrsdk-update.sh
+++ scripts/gvrsdk-update.sh
@@ -28,109 +28,109 @@ unzip gvr-android-sdk-$ver/libraries/sdk-audio-$ver.aar -d audio.aar
 
 ################################################
 # Base package
-mkdir vr_base
+mkdir sdk-base
 
 # Start by copying manifest and resources
-cp base.aar/AndroidManifest.xml vr_base
-cp -R base.aar/res/ vr_base/res
+cp base.aar/AndroidManifest.xml sdk-base
+cp -R base.aar/res/ sdk-base/res
 
 # Extract classes from library's jar
 unzip base.aar/classes.jar -d base.aar/classes
-mkdir vr_base/libs
-mkdir vr_base/libs/base
+mkdir sdk-base/libs
+mkdir sdk-base/libs/base
 # Copy classes
-cp -R base.aar/classes/com vr_base/libs/base/com
+cp -R base.aar/classes/com sdk-base/libs/base/com
 # Copy native libs
-cp -R base.aar/jni vr_base/libs/base/lib
+cp -R base.aar/jni sdk-base/libs/base/lib
 # Create the jar file
-jar cf vr_base-classes.jar -C vr_base/libs/base .
+jar cf gvr-base.jar -C sdk-base/libs/base .
 
 # Remove original folder and put jar file in its final location
-rm -Rf vr_base/libs/base
-mv vr_base-classes.jar vr_base/libs
+rm -Rf sdk-base/libs/base
+mv gvr-base.jar sdk-base/libs
 
 # Need the jar also in cardboard's lib folder
-cp vr_base/libs/vr_base-classes.jar ../libraries/cardboard/lib
+cp sdk-base/libs/gvr-base.jar ../libraries/cardboard/lib
 
 # Finally, create zip file and mode to the sdk location
-zip -r vr_base.zip vr_base
-mv vr_base.zip ../libraries/cardboard/gvrsdk/$ver
+zip -r sdk-base.zip sdk-base
+mv sdk-base.zip ../libraries/cardboard/gvrsdk/$ver
 
 
 ################################################
 # Common package
-mkdir vr_common
+mkdir sdk-common
 
 # Start by copying manifest and resources
-cp common.aar/AndroidManifest.xml vr_common
-cp -R common.aar/res/ vr_common/res
+cp common.aar/AndroidManifest.xml sdk-common
+cp -R common.aar/res/ sdk-common/res
 
 # Extract classes from library's jar
 unzip common.aar/classes.jar -d common.aar/classes
-mkdir vr_common/libs
-mkdir vr_common/libs/common
+mkdir sdk-common/libs
+mkdir sdk-common/libs/common
 # Copy classes
-cp -R common.aar/classes/com vr_common/libs/common/com
+cp -R common.aar/classes/com sdk-common/libs/common/com
 # Create the jar file
-jar cf vr_common-classes.jar -C vr_common/libs/common .
+jar cf gvr-common.jar -C sdk-common/libs/common .
 
 # Remove original folder and put jar file in its final location
-rm -Rf vr_common/libs/common
-mv vr_common-classes.jar vr_common/libs
+rm -Rf sdk-common/libs/common
+mv gvr-common.jar sdk-common/libs
 
 # protobuf-javanano was unbundled from the SDK:
 # https://github.com/googlevr/gvr-android-sdk/issues/264
-# but required by vr_common, the correspond pom file alongside the aar contains the 
+# but required by sdk-common, the correspond pom file alongside the aar contains the 
 # details of the dependency.
 wget http://central.maven.org/maven2/com/google/protobuf/nano/protobuf-javanano/$nano/protobuf-javanano-$nano.jar
-mv protobuf-javanano-$nano.jar vr_common/libs
+mv protobuf-javanano-$nano.jar sdk-common/libs
 
 # Need the jar also in cardboard's lib folder
-cp vr_common/libs/vr_common-classes.jar ../libraries/cardboard/lib
+cp sdk-common/libs/gvr-common.jar ../libraries/cardboard/lib
 
 # Finally, create zip file and mode to the sdk location
-zip -r vr_common.zip vr_common
-mv vr_common.zip ../libraries/cardboard/gvrsdk/$ver
+zip -r sdk-common.zip sdk-common
+mv sdk-common.zip ../libraries/cardboard/gvrsdk/$ver
 
 ################################################
 # Audio package
-mkdir vr_audio
+mkdir sdk-audio
 
 # Start by copying manifest and resources
-cp audio.aar/AndroidManifest.xml vr_audio
-cp -R audio.aar/res/ vr_audio/res
+cp audio.aar/AndroidManifest.xml sdk-audio
+cp -R audio.aar/res/ sdk-audio/res
 
 # Extract classes from library's jar
 unzip audio.aar/classes.jar -d audio.aar/classes
-mkdir vr_audio/libs
-mkdir vr_audio/libs/audio
+mkdir sdk-audio/libs
+mkdir sdk-audio/libs/audio
 # Copy classes
-cp -R audio.aar/classes/com vr_audio/libs/audio/com
+cp -R audio.aar/classes/com sdk-audio/libs/audio/com
 # Copy native libs
-cp -R audio.aar/jni vr_audio/libs/audio/lib
+cp -R audio.aar/jni sdk-audio/libs/audio/lib
 # Create the jar file
-jar cf vr_audio-classes.jar -C vr_audio/libs/audio .
+jar cf gvr-audio.jar -C sdk-audio/libs/audio .
 
 # Remove original folder and put jar file in its final location
-rm -Rf vr_audio/libs/audio
-mv vr_audio-classes.jar vr_audio/libs
+rm -Rf sdk-audio/libs/audio
+mv gvr-audio.jar sdk-audio/libs
 
 # Need the jar also in cardboard's lib folder
-cp vr_audio/libs/vr_audio-classes.jar ../libraries/cardboard/lib
+cp sdk-audio/libs/gvr-audio.jar ../libraries/cardboard/lib
 
 # Finally, create zip file and mode to the sdk location
-zip -r vr_audio.zip vr_audio
-mv vr_audio.zip ../libraries/cardboard/gvrsdk/$ver
+zip -r sdk-audio.zip sdk-audio
+mv sdk-audio.zip ../libraries/cardboard/gvrsdk/$ver
 
 ################################################
 # Cleanup
-rm -Rf vr_base
+rm -Rf sdk-base
 rm -Rf base.aar
 
-rm -Rf vr_common
+rm -Rf sdk-common
 rm -Rf common.aar
 
-rm -Rf vr_audio
+rm -Rf sdk-audio
 rm -Rf audio.aar
 
 rm -Rf gvr-android-sdk-$ver
