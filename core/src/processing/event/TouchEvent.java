e3d0c0cdd07c6a1f961ef3a29897512a5075b639
codeanticode <andres.colubri@gmail.com>
2017-01-15 15:31:09 -0500

    starting to work on the multitouch functionality

--- core/src/processing/event/TouchEvent.java
+++ core/src/processing/event/TouchEvent.java
@@ -41,7 +41,6 @@ pan
 longpress
 */
 public class TouchEvent extends Event {
-
   public TouchEvent(Object nativeObject, long millis, int action, int modifiers) {
     super(nativeObject, millis, action, modifiers);
     this.flavor = TOUCH;
