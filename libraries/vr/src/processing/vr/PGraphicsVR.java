409925acff53bd7f99c14eda334fad318b22e64a
codeanticode <andres.colubri@gmail.com>
2017-04-25 00:25:28 -0400

    remove updateGLNormal from PGraphicsVR

--- libraries/vr/src/processing/vr/PGraphicsVR.java
+++ libraries/vr/src/processing/vr/PGraphicsVR.java
@@ -211,30 +211,6 @@ public class PGraphicsVR extends PGraphics3D {
   }
 
 
-  @Override
-  protected void updateGLNormal() {
-    if (glNormal == null) {
-      glNormal = new float[9];
-    }
-
-    if (glCoordsEnabled) {
-      // Since the coordinate system is right-handed, we need to invert the normal calculation so
-      // lighting works as it should, even if we provide the normals as before.
-      glNormal[0] = -modelviewInv.m00;
-      glNormal[1] = -modelviewInv.m01;
-      glNormal[2] = -modelviewInv.m02;
-
-      glNormal[3] = -modelviewInv.m10;
-      glNormal[4] = -modelviewInv.m11;
-      glNormal[5] = -modelviewInv.m12;
-
-      glNormal[6] = -modelviewInv.m20;
-      glNormal[7] = -modelviewInv.m21;
-      glNormal[8] = -modelviewInv.m22;
-    }
-  }
-
-
   protected void setVRViewport() {
     pgl.viewport(eyeViewport.x, eyeViewport.y, eyeViewport.width, eyeViewport.height);
   }
