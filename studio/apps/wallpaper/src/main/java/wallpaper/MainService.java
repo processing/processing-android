02e6cacd69dd90083b9a4838d159ccf0bfca779c
codeanticode <andres.colubri@gmail.com>
2018-08-09 13:26:58 -0400

    add waitForDebugger() call in wallpaper's main service

--- studio/apps/wallpaper/src/main/java/wallpaper/MainService.java
+++ studio/apps/wallpaper/src/main/java/wallpaper/MainService.java
@@ -6,6 +6,8 @@ import processing.core.PApplet;
 public class MainService extends PWallpaper {  
   @Override
   public PApplet createSketch() {
+    // Uncomment the following line when debugging:
+//    android.os.Debug.waitForDebugger();
     PApplet sketch = new Sketch();    
     return sketch;
   }
