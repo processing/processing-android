f33e0264f991b08aad88aa2e43624cf4d2f6bc3e
codeanticode <andres.colubri@gmail.com>
2019-01-05 14:05:33 +0900

    removed obj file from test app

--- studio/apps/arscene/src/main/java/arscene/Sketch.java
+++ studio/apps/arscene/src/main/java/arscene/Sketch.java
@@ -3,7 +3,6 @@ package arscene;
 import processing.core.PApplet;
 import processing.ar.*;
 import processing.ar.render.*;
-import processing.core.PShape;
 
 public class Sketch extends PApplet {
   public void settings() {
@@ -18,14 +17,7 @@ public class Sketch extends PApplet {
   public void draw() {
     lights();
     background(0);
-//    PPlane.setPlaneColor(0x00BCD4FF);
-//    shape(sphere);
-
     fill(0x769FE0);
-    // Pulsating box to test scale...
-//    box(map(sin(frameCount/100.0f), -1, 1, 0, 500));
-    //box(0.25f);
-
     sphere(0.10f);
     rotateZ(frameCount * 0.1f);
     translate(0, 0.3f,0);
