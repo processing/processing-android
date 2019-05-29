27fc6d33fd3b9a570509ae9e3d79dbd7e5cd1df7
codeanticode <andres.colubri@gmail.com>
2019-05-29 02:44:48 -0400

    add resources

--- mode/libraries/ar/src/processing/ar/BackgroundRenderer.java
+++ mode/libraries/ar/src/processing/ar/BackgroundRenderer.java
@@ -6,6 +6,9 @@ import android.opengl.GLES20;
 
 import com.google.ar.core.Frame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
