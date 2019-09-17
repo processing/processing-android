81236f5cee84db9367a89e412b119de96997fcc3
codeanticode <andres.colubri@gmail.com>
2019-09-17 11:28:46 -0400

    put spaces back

--- mode/src/processing/mode/android/AndroidPreprocessor.java
+++ mode/src/processing/mode/android/AndroidPreprocessor.java
@@ -1,10 +1,13 @@
 /*
  Part of the Processing project - http://processing.org
+
  Copyright (c) 2012-17 The Processing Foundation
  Copyright (c) 2009-12 Ben Fry and Casey Reas
+
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License version 2
  as published by the Free Software Foundation.
+ 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
