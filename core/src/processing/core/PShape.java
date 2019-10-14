cb8c058b09915b76ec5ad04e0ab51c199936d387
codeanticode <andres.colubri@gmail.com>
2019-10-13 22:41:05 -0400

    implementation of contains() in PShape same as in Java mode

--- core/src/processing/core/PShape.java
+++ core/src/processing/core/PShape.java
@@ -2822,22 +2822,48 @@ public class PShape implements PConstants {
   // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 
-  // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+  /**
+   * Return true if this x, y coordinate is part of this shape. Only works
+   * with PATH shapes or GROUP shapes that contain other GROUPs or PATHs.
+   */
   public boolean contains(float x, float y) {
     if (family == PATH) {
+      PVector p = new PVector(x, y);
+      if (matrix != null) {
+        // apply the inverse transformation matrix to the point coordinates
+        PMatrix inverseCoords = matrix.get();
+        // TODO why is this called twice? [fry 190724]
+        // commit was https://github.com/processing/processing/commit/027fc7a4f8e8d0a435366eae754304eea282512a
+        inverseCoords.invert();  // maybe cache this?
+        inverseCoords.invert();  // maybe cache this?
+        inverseCoords.mult(new PVector(x, y), p);
+      }
+
+      // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
       boolean c = false;
       for (int i = 0, j = vertexCount-1; i < vertexCount; j = i++) {
-        if (((vertices[i][Y] > y) != (vertices[j][Y] > y)) &&
-            (x <
+        if (((vertices[i][Y] > p.y) != (vertices[j][Y] > p.y)) &&
+            (p.x <
                 (vertices[j][X]-vertices[i][X]) *
-                (y-vertices[i][Y]) /
-                (vertices[j][1]-vertices[i][Y]) +
-                vertices[i][X])) {
+                    (y-vertices[i][Y]) /
+                    (vertices[j][1]-vertices[i][Y]) +
+                    vertices[i][X])) {
           c = !c;
         }
       }
       return c;
+
+    } else if (family == GROUP) {
+      // If this is a group, loop through children until we find one that
+      // contains the supplied coordinates. If a child does not support
+      // contains() throw a warning and continue.
+      for (int i = 0; i < childCount; i++) {
+        if (children[i].contains(x, y)) return true;
+      }
+      return false;
+
     } else {
+      // https://github.com/processing/processing/issues/1280
       throw new IllegalArgumentException("The contains() method is only implemented for paths.");
     }
   }
