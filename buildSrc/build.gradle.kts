5fe1455cf1916375b7cafe23dbc0e415a58a5f9f
ranaaditya <mail2aditya.rana98@gmail.com>
2020-08-01 19:09:55 +0530

    Add support for dokka in Plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,22 +1,30 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     groovy
     kotlin("jvm") version ("1.3.72")
+    id("org.jetbrains.dokka") version("0.10.1")
 }
 
+val dokka_version = "0.10.1"
+
 buildscript {
+    val dokka_version = "0.10.1"
     repositories {
         mavenCentral()
         jcenter()
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
     dependencies {
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:$dokka_version")
     }
 }
 
 repositories {
     google()
     jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 dependencies {
@@ -24,4 +32,10 @@ dependencies {
     implementation(localGroovy())
     implementation("com.android.tools.build:gradle:3.5.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.72")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$dokka_version")
+}
+
+val dokka by tasks.getting(DokkaTask::class) {
+    outputDirectory = "$buildDir/dokka"
+    outputFormat = "html"
 }
