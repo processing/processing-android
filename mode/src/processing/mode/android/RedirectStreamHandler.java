a43289316767919ab56df19d0769597ea486e456
Rupesh Kumar <46577873+rupeshkumar22@users.noreply.github.com>
2022-07-30 20:54:09 +0530

    Update RedirectStreamHandler.java

--- mode/src/processing/mode/android/RedirectStreamHandler.java
+++ mode/src/processing/mode/android/RedirectStreamHandler.java
@@ -1,14 +1,10 @@
 package processing.mode.android;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class RedirectStreamHandler extends Thread {
