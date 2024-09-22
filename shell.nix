710db148541bb9a7c068d397152d88b2117b4ad4
phanirithvij <phanirithvij2000@gmail.com>
2024-09-22 17:11:59 +0530

    nix shell w/ npins
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ shell.nix
@@ -0,0 +1,31 @@
+let
+  sources = import ./npins;
+  pkgs = import sources.nixpkgs {
+    config.android_sdk.accept_license = true;
+  };
+  jdk = pkgs.jdk17_headless;
+  # note: can use pkgs.androidenv.androidPkgs.androidsdk
+  # but it pulls in lots of toolchains see https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/default.nix#L18
+  androidSdk' =
+    (pkgs.androidenv.composeAndroidPackages {
+      platformVersions = [ "33" ];
+      includeEmulator = false;
+      includeSystemImages = false;
+      includeNDK = false;
+    }).androidsdk;
+in
+(pkgs.buildFHSUserEnv {
+  name = "android-sdk";
+  targetPkgs =
+    pkgs:
+    (with pkgs; [
+      androidSdk'
+      glibc
+      nixfmt-rfc-style
+    ]);
+  runScript = pkgs.writeScript "init.sh" ''
+    export JAVA_HOME=${jdk}
+    export PATH="${jdk}/bin:$PATH"
+    exec bash
+  '';
+}).env
